313f26329e2e1a35a1b86aaa7bfad16c721997d7
Alvaro Jose Peralta Ocampo <alvaro.peralta@fch.cl>
2023-10-05 16:21:30 -0300

    actualizar instancia de prueba

--- src/tablero/TestGame.java
+++ src/tablero/TestGame.java
@@ -16,19 +16,22 @@ class TestGame {
 	
 	@Test
 	void testJuegoPapel() {
-		assertTrue(new Papel().play(new Tijera()) == 0);
+		assertTrue(new Papel().play(new Papel()) == 0);
+		assertTrue(new Papel().play(new Tijera()) == -1);
 		assertTrue(new Papel().play(new Piedra()) == 1);
 	}
 	
 	@Test
 	void testJuegoTijera() {
+		assertTrue(new Tijera().play(new Tijera()) == 0);
 		assertTrue(new Tijera().play(new Papel()) == 1);
-		assertTrue(new Tijera().play(new Piedra()) == 0);
+		assertTrue(new Tijera().play(new Piedra()) == -1);
 	}
 
 	@Test
 	void testJuegoPiedra() {
-		assertTrue(new Piedra().play(new Papel()) == 0);
+		assertTrue(new Piedra().play(new Piedra()) == 0);
+		assertTrue(new Piedra().play(new Papel()) == -1);
 		assertTrue(new Piedra().play(new Tijera()) == 1);
 	}
 
